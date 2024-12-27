06f22fd08d47c2cca6ab17ef843ddfe82ee9123f
Molnia00 <bilankati@gmail.com>
2024-12-27 16:20:51 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import Profile from './components/Profile/Profile'
-import userData from "./userData.json";
+import userData from './userData.json';
+import FriendList from './components/FriendList/FriendList'
+import friends from './friends.json';
 //
 
 
@@ -14,6 +16,7 @@ function App () {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
   </>
   );
 };
